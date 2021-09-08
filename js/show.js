4c779ec91d428e24df126da24aeee74b5f6a41ad
JIN JIAJIE <JJJinGinger@gmail.com>
2021-09-08 21:56:52 +0900

    new article

--- js/show.js
+++ js/show.js
@@ -1,5 +1,10 @@
-function show()
+function show(name)
 {
+	if ($('#for_set_text')) //初始化
+	{
+		$('#for_set_text').remove();
+	}
 
-
+	$('#'+name).after('<div id="for_set_text" class="container"></div>');
+	$('#for_set_text').append('<object data="text/' + name + '.html" class="sch_text"></object>');
 };
