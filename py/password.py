8a4c17583cd2d42ca855a3edba1c04f1d21a4b1e
JIN JIAJIE <JJJinGinger@gmail.com>
2023-12-22 15:12:08 +0900

    diar css

--- py/password.py
+++ py/password.py
@@ -4,8 +4,7 @@ import json
 
 app = Flask(__name__, static_folder='../', template_folder='../')
 
-# 假设密码存储在环境变量或者安全的配置文件中
-PASSWORD = 'qwertyuiop123'  # 这里设置你的密码，实际应用中应该使用加密密码
+PASSWORD = 'qwertyuiop123'
 
 @app.route('/login', methods=['POST'])
 def login():
@@ -15,11 +14,10 @@ def login():
     else:
         return jsonify({'success': False}), 401
 
-# 日记页面路由
 @app.route('/diaries.html')
 def diaries_page():
-    # 这里应该有验证用户已经登录的逻辑
-    return '这是日记页面'
+
+    return '------'
 
 if __name__ == '__main__':
     app.run(debug=True)
